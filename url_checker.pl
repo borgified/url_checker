840d5778542387d968b5d5db95c8a08ff63732df
Fwiffo <borgified@gmail.com>
2014-04-23 17:42:04 -0700

    removed pithub dependencies
    we arent querying github anymore to extract committer data

--- url_checker.pl
+++ url_checker.pl
@@ -3,13 +3,8 @@
 use warnings;
 use strict;
 use LWP::UserAgent;
-use Pithub;
 use HTTP::Cookies;
-
 use URI::Find::UTF8;
-
-#github accounts/passwords config
-my %config = do '/secret/github.config';
 
 #check if we have the original vhf/free-programming-books repo pulled down
 #if not, do it, if so, grab the latest version
@@ -57,8 +52,6 @@ $ua->agent("Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2.13) Gecko/2
 $ua->timeout(120);
 $ua->cookie_jar($cookies);
 #$ua->show_progress(1);
-
-my $c = Pithub::Repos::Commits->new( token => $config{'token'});
 
 $|=1;
 foreach my $book (keys %db){
@@ -123,65 +116,4 @@ sub test_url{
 		}
 	}
 	return $retval;
-}
-
-
-
-__END__
-
-
-my $test = &test_url($url);
-
-if($test ne 'good'){
-my $lang="en";
-if($book =~ /free-programming-books-(.*)\.md/){
-$lang=$1;
-}elsif($book eq 'javascript-frameworks-resources.md'){
-$lang='jfr';
-}elsif($book eq 'free-programming-interactive-tutorials-en.md'){
-$lang='fpite';
-}
-
-#figure out which commit added this bad url
-my $commit = `git log -S$url --reverse --format=%H | head -1`;
-chomp($commit);
-
-#figure out the commit's author's login so we take a closer
-#look at this single commit and look for the login info
-my $cc = $c->get(
-user => 'vhf',
-repo => 'free-programming-books',
-sha  => $commit,
-);
-my $committer = $cc->content->{author}->{login};
-
-print "$test\nIt was added by \@$committer in $commit. label: $lang\t title: $url\n";
-
-}
-}
-}
-}
-
-sub test_url{
-my $retval;
-my $url = shift @_;
-my $req = HTTP::Request->new(HEAD => $url);
-my $res = $ua->request($req);
-
-### CHANGE HERE TO THE MAKE HE CHECK LESS STRINGENT 
-
-if($res->is_success){
-$retval = "good";
-}else{
-#try one more time with GET
-$req = HTTP::Request->new(GET => $url);
-$ua->timeout(360); #wait 6 mins in case it's really slow
-$res = $ua->request($req);
-if($res->is_success){
-$retval = "good";
-}else{
-$retval = $res->status_line."\n$url";
-}
-}
-return $retval;
 }
